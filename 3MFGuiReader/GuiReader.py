6d1e33e345b9504bd1093378632262de65547876
George Krommydas <geokrom98@gmail.com>
2023-02-13 15:09:43 +0200

    Add files via upload

--- 3MFGuiReader/GuiReader.py
+++ 3MFGuiReader/GuiReader.py
@@ -1,19 +1,26 @@
 # Copyright (c) 2022 Ultimaker B.V.
-# Cura is released under the terms of the LGPLv3 or higher.
+# Cura is released under the terms of the LGPLv3 or higher. #\
 
-from cura import cura.CuraApplication
+from Cura.cura import CuraApplication
+from Uranium.UM.View import View
+from Uranium.UM.Logger import Logger
 
-from UM.Logger import Logger
 try:
     from . import ThreeMFReader
     from . import ThreeMFWorkspaceReader
 except ImportError:
     Logger.log("w", "Could not import ThreeMFReader and ThreeMFWorkspaceReader; libSavitar may be missing")
 
-class  ViewMetaData:
-
-    def __init__(self):
-
-
 class GuiReader:
-    pass
+
+    def __init__(self, metaData, path):
+        self.metaData = {}
+        self.path = path
+
+
+    def getMetaData(self, metadata):
+
+        for key, attr in enumerate(metadata):
+            attribute_value = attr[key]
+            if attribute_value:
+                self.metadata = attribute_value
